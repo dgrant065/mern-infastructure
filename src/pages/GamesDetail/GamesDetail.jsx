1a36f5cf44c6e385b6170f0d50220008b30f2353
Darius Grant <d.grant065@gmail.com>
2023-10-15 21:09:52 -0400

    Details page

--- src/pages/GamesDetail/GamesDetail.jsx
+++ src/pages/GamesDetail/GamesDetail.jsx
@@ -21,7 +21,10 @@ export default function GamesDetail() {
     <div id='board'>
         {game.det}
         <div className='image'><img src={game.background_image}/></div>
+        <h1>Metacritic Score:</h1><p>{game.metacritic}</p>
+        <h2>Released:</h2><p>{game.released}</p>
         <p>{game.description_raw}</p>
+        <h3>Website:</h3><p>{game.website}</p>
         </div>
     
   )
